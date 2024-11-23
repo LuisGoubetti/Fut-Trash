88feeb42561f89015a6d395a17a6427301ecf39b
Gabriel Felipe Floriano <143909768+GabrielFFloriano@users.noreply.github.com>
2024-11-22 21:43:58 -0300

    Update JanelaDoJogo.java

--- JanelaDoJogo.java
+++ JanelaDoJogo.java
@@ -2,8 +2,9 @@ import javax.swing.JFrame;
 
 public class JanelaDoJogo extends JFrame{
     public JanelaDoJogo(){
-        this.setTitle("Invasão Alienígena");
-        this.setSize(800,600);
+        this.setTitle("Fut-Trash");
+        this.setSize(1080,720);
+        this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.add(new PainelDoJogo());
         this.setVisible(true);
